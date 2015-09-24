5905bc8c27493a3ef4038574cae213e7ee29900b
GIlyass <i.garara@gmail.com>
2015-09-24 13:56:49 +0200

    Implement the basic layout for the client Android app

---
+++ MoNADClient/app/src/main/java/se/uu/csproject/monadclient/NotificationsActivity.java
@@ -0,0 +1,75 @@
+package se.uu.csproject.monadclient;
+
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+        toolbar = (Toolbar) findViewById(R.id.actionToolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if(id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        if (id == R.id.action_search) {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+        if (id == R.id.action_notifications) {
+            return true;
+        }
+
+        if (id == R.id.action_mytrips) {
+            startActivity(new Intent(this, TripsActivity.class));
+        }
+
+        if (id == R.id.action_profile) {
+            startActivity(new Intent(this, ProfileActivity.class));
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        }
+
+        if (id == R.id.action_aboutus) {
+            //TODO: Create a toaster with text about the MoNAD project and team
+        }
+
+        if (id == R.id.action_signout) {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
