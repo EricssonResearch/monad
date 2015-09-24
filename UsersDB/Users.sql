2cedf71558390cf2d00519711177086c1bbc8af3
csioa <babispasg@gmail.com>
2015-09-24 15:25:36 +0200

    Connection's password and number of users given in the CL

--- UsersDB/Users.sql
+++ UsersDB/Users.sql
@@ -29,6 +29,9 @@ CREATE TABLE VEHICLE(
 
 CREATE TABLE DRIVER(
 	username varchar(255) NOT NULL,
+	firstname varchar(255) 	NOT NULL,
+	lastname varchar(255) NOT NULL,
+	email varchar(255) 	NOT NULL,
 	pass char(40),
 	PRIMARY KEY(username)
 );
@@ -36,17 +39,21 @@ CREATE TABLE DRIVER(
 ALTER TABLE VEHICLE AUTO_INCREMENT = 100;
 ALTER TABLE VEHICLE ADD FOREIGN KEY(current_driver) REFERENCES DRIVER(username);
 
--- pass (password) field depends on the hashing algorithm. e.g. here is SHA-1
--- role is either 0 for a simple user/passenger or '1' for driver. we presume that
--- the default is a user/passenger otherwise we have to explicitly change it
+/*
+pass (password) field depends on the hashing algorithm. e.g. here is SHA-1
+ role is either 0 for a simple user/passenger or '1' for driver. we presume that
+ the default is a user/passenger otherwise we have to explicitly change it
 
--- same with the activated field where the default is '0' (not activated) and as soon
--- as the user activates his/her account it is updated to '1'
+ same with the activated field where the default is '0' (not activated) and
+ as soon  as the user activates his/her account it is updated to '1'
+*/
 
 DELIMITER $$
 
-CREATE FUNCTION `SIGNUP` (uname VARCHAR(255),fname VARCHAR(255), lname VARCHAR(255), em VARCHAR(255),  ph VARCHAR(15), pass VARCHAR(255))
-RETURNS INTEGER
+CREATE FUNCTION `SIGNUP` (uname VARCHAR(255),fname VARCHAR(255),
+						  lname VARCHAR(255), em VARCHAR(255),
+						  ph VARCHAR(15), pass VARCHAR(255))
+RETURNS SMALLINT
 BEGIN
 	IF EXISTS(SELECT * FROM USER_PROFILE WHERE username = uname)
 	THEN
@@ -58,7 +65,8 @@ BEGIN
 	THEN
 		RETURN 2;
 	ELSE
-		INSERT INTO USER_PROFILE (username, firstname, lastname, email, phone, pass) VALUES (uname, fname, lname, em, ph, sha1(pass));
+		INSERT INTO USER_PROFILE (username, firstname, lastname, email, phone, pass)
+		VALUES (uname, fname, lname, em, ph, sha1(pass));
 		RETURN 3;
 	END IF;
 END $$
@@ -66,9 +74,10 @@ END $$
 -- DELIMITER $$
 
 CREATE FUNCTION `SIGNIN` (uname VARCHAR(255), pass VARCHAR(255))
-RETURNS bit
+RETURNS BIT
 BEGIN
-	IF EXISTS(SELECT * FROM USER_PROFILE WHERE username = uname AND sha1(pass) = pass)
+	IF EXISTS(SELECT * FROM USER_PROFILE WHERE username = uname
+										 AND sha1(pass) = pass)
 	THEN
 		RETURN 1;
 	ELSE
@@ -81,20 +90,23 @@ DELIMITER ;
 -- creating the first driver
 /*
 INSERT INTO USER_PROFILE(username, firstname, lastname, email, phone, pass)
-VALUES('chge','Charalampos', 'Georgiadis', 'babispasg@gmail.com', '1234567890', 'mypass');
+VALUES('chge','Charalampos', 'Georgiadis', 'babispasg@gmail.com', '1234567890',
+			 'mypass');
 
 INSERT INTO USER_PROFILE(username, firstname, lastname, email, phone, pass)
-VALUES('chko', 'Charalampos', 'Kominos', 'charis@gmail.com', '0987654321', '1234');
+VALUES('chko', 'Charalampos', 'Kominos', 'charis@gmail.com', '0987654321',
+			 '1234');
 
 INSERT INTO DRIVER(username) VALUES ('driver1');
 
 INSERT INTO VEHICLE(licence, current_driver) VALUES ('1324756890', 'driver1');
 
-SELECT SIGNUP('cGge','Charalampos', 'Georgiadis', 'babispas@gmail.com', '1234567891', 'mypassword');
+SELECT SIGNUP('cGge','Charalampos', 'Georgiadis', 'babispas@gmail.com',
+							'1234567891', 'mypassword');
 SELECT SIGNIN('chge','mypass');
 */
 
--- SELECT COUNT(*) FROM USER_PROFILE WHERE firstname = 'Ciera';
+-- SELECT * FROM USER_PROFILE;
 -- SELECT * FROM VEHICLE;
 
 
--- UsersDB/populator.py
+++ UsersDB/populator.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
+import sys
 import uuid
 import random
 import MySQLdb
 
-NUMBER_OF_USERS = 100000
+''' Populator for our DB
+
+A simple populator that takes a NUMBER_OF_USERS and creates a number of
+random dummy entries for our USER_PROFILE table. To run for your localhost
+execute:
+
+python populator.py <connection_password> <number_of_users>
+'''
+
+# Select a random number of entries. Default is 1000
+NUMBER_OF_USERS = int(sys.argv[2])
 
 #DATABASE CREDENTIALS
 HOST = "localhost"
 USER = "root"
-PASSWORD = ""
+# Change the password to connect to your database
+PASSWORD = str(sys.argv[1])
 DB = "MONAD"
 
 # Open database connection
@@ -18,43 +30,55 @@ db = MySQLdb.connect(HOST,USER,PASSWORD,DB)
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
+# Random names list
 firstnames = ['Ciera', 'Mariah', 'Jaimie','Ricky', 'Roselle','Summer',
     'Gail', 'Madge', 'Jennell', 'Emilio', 'Ranae', 'Dora', 'Virginia', 'Ursula',
-    'Timika', 'Rheba', 'Karon', 'Ute', 'Elisabeth', 'Jolyn', 'Shirleen', 'Shirly',
-    'Lenny', 'Willena', 'Latrisha', 'Edison', 'Claudio', 'Felisa', 'Domenica', 'Cherri',
-    'Nina', 'Norah', 'Velma', 'Benito', 'Lorene', 'Sharan', 'Donna', 'Lizeth',
-    'Sona', 'Latrina', 'Frank', 'Rudolph', 'Angeline', 'Sanora', 'Anna', 'Madeline',
-    'Margarete', 'Zina', 'Kassie', 'Dewey', 'Zana', 'Sharee', 'Rayford', 'Ashlie',
-    'Brittanie', 'Calvin', 'Joselyn', 'Valeria', 'Kamilah', 'Jenelle', 'Paula', 'Azalee',
-    'Hayden', 'Madlyn', 'Asa', 'Natasha', 'Sheron', 'Josef', 'Gisele', 'Lauretta',
-    'Millie', 'Edra', 'Nathanial', 'Camelia', 'Burt', 'Grady', 'Augustus', 'Cathleen',
-    'Lani', 'Elliot', 'Nona', 'Lilliana', 'Perry', 'Waltraud', 'Felicidad', 'Hershel',
-    'Margaretta', 'Lemuel', 'Jamila', 'Jae', 'Faith', 'Marleen', 'Mabel', 'Virgina',
-    'Arvilla', 'Sena', 'Marlen', 'Brynn', 'Mark', 'Milagro',
+    'Timika', 'Rheba', 'Karon', 'Ute', 'Elisabeth', 'Jolyn', 'Shirleen',
+    'Shirly',     'Lenny', 'Willena', 'Latrisha', 'Edison', 'Claudio', 'Felisa',
+    'Domenica', 'Cherri', 'Nina', 'Norah', 'Velma', 'Benito', 'Lorene',
+    'Sharan', 'Donna', 'Lizeth',     'Sona', 'Latrina', 'Frank', 'Rudolph',
+    'Angeline', 'Sanora', 'Anna', 'Madeline', 'Margarete', 'Zina', 'Kassie',
+    'Dewey', 'Zana', 'Sharee', 'Rayford', 'Ashlie',     'Brittanie', 'Calvin',
+    'Joselyn', 'Valeria', 'Kamilah', 'Jenelle', 'Paula', 'Azalee', 'Hayden',
+    'Madlyn', 'Asa', 'Natasha', 'Sheron', 'Josef', 'Gisele', 'Lauretta',
+    'Millie', 'Edra', 'Nathanial', 'Camelia', 'Burt', 'Grady', 'Augustus',
+    'Cathleen',     'Lani', 'Elliot', 'Nona', 'Lilliana', 'Perry', 'Waltraud',
+    'Felicidad', 'Hershel',     'Margaretta', 'Lemuel', 'Jamila', 'Jae',
+    'Faith', 'Marleen', 'Mabel', 'Virgina', 'Arvilla', 'Sena', 'Marlen',
+    'Brynn', 'Mark', 'Milagro'
 ]
 
-domains = [ "hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
+# Random emails provider and name list
+domains = [ "hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz",
+            "yahoo.com"]
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
 
+# Function for generating random email addresses
 def random_email():
-    name = ''.join(letters[random.randint(0, len(letters)-1)] for i in range (7))
+    name = ( ''.join(letters[random.randint(0, len(letters)-1)] for i in
+             range (7)))
     name += "@"
     name += domains[random.randint(0, len(domains)-1)]
     return name
 
+# Function for generating random unique usernames, phones, names and passwords
+# and populating our table with all the credentials by calling the SIGNUP
+# function
 def populate_users(number):
     phone = 1000000000
     for i in range(number):
-        sql = ("SELECT SIGNUP('%s','%s','%s','%s','%s','%s')" %(uuid.uuid4(),
-                                                                firstnames[random.randint(0,len(firstnames)-1)],
-                                                                firstnames[random.randint(0,len(firstnames)-1)],
-                                                                random_email(),
-                                                                phone,
-                                                                uuid.uuid4()))
+        sql = (("SELECT SIGNUP('%s','%s','%s','%s','%s','%s')"
+                %(uuid.uuid4(),
+                  firstnames[random.randint(0,len(firstnames)-1)],
+                  firstnames[random.randint(0,len(firstnames)-1)],
+                  random_email(),
+                  phone,
+                  uuid.uuid4())))
         #print sql
         cursor.execute(sql)
         phone += 1
 
+# The actual call of the function
 sql = "SET autocommit = 0;"
 cursor.execute(sql)
 sql = "SET unique_checks=0;"
@@ -66,7 +90,6 @@ sql = "SET unique_checks=1;"
 cursor.execute(sql)
 sql = "COMMIT;"
 cursor.execute(sql)
-
 
 # disconnect from server
 
