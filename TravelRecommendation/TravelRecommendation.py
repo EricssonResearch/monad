8027885503590b1d1cbf2f11102d9b3d561e130b
csioa <babispasg@gmail.com>
2015-10-13 15:39:47 +0200

    Returns a fixed number of recommendations (3) for each centroid of the user's past request clusters. Also matches each centroid with all the possible combinations of routes, including the intermediate waypoints

--- TravelRecommendation/TravelRecommendation.py
+++ TravelRecommendation/TravelRecommendation.py
@@ -1,4 +1,5 @@
 import numpy
+import math
 from pyspark import SparkContext
 from pymongo import MongoClient
 from pyspark.mllib.clustering import KMeans, KMeansModel
@@ -13,41 +14,75 @@ db = client.monad
 TravelRequest = db.TravelRequest
 TimeTable = db.TimeTable
 
-#sc = SparkContext()
+# Not needed if executed from pyspark. We need it only for ./bin/spark-submit
+sc = SparkContext()
 
 # TODO Specify the user we want to look for
 results = TravelRequest.find()
 
 users = []
 
-# Number of iterations to find the best k
+# Number of iterations to find the best k, minimum and maximum latitude,
+# longitude and radius and number of recommendations for each cluster
 NUM_OF_IT = 8
 MIN_LATITUDE = 59.78
 MAX_LATITUDE = 59.92
 MIN_LONGITUDE = 17.53
 MAX_LONGITUDE = 17.75
-MIN_TIME = 0
-MAX_TIME = 43200
+MIN_COORDINATE = -13750
+MAX_COORDINATE = 13750
+CIRCLE_CONVERTER = math.pi / 43200
+NUMBER_OF_RECOMMENDATIONS = 3
+
+# Converting time object to seconds
+def toSeconds(dt):
+    total_time = dt.hour * 3600 + dt.minute * 60 + dt.second
+    return total_time
+
+# Mapping seconds value to (x, y) coordinates
+def toCoordinates(secs):
+    angle = float(secs) * CIRCLE_CONVERTER
+    x = 13750 * math.cos(angle)
+    y = 13750 * math.sin(angle)
+    return x, y
+
+# Normalization functions
+def timeNormalizer(value):
+    new_value = float((float(value) - MIN_COORDINATE) /
+                      (MAX_COORDINATE - MIN_COORDINATE))
+    return new_value
+
+def latNormalizer(value):
+    new_value = float((float(value) - MIN_LATITUDE) /
+                      (MAX_LATITUDE - MIN_LATITUDE))
+    return new_value
+
+def lonNormalizer(value):
+    new_value = float((float(value) - MIN_LONGITUDE) /
+                      (MAX_LONGITUDE - MIN_LONGITUDE))
+    return new_value
 
 # Creates a list with the data retrieved from MongoDB for the selected user
 for res in results:
     users.append((res['start_position_lat'], res['start_position_lon'],
-    res['end_position_lat'], res['end_position_lon'])) #(res['start_time']).time(),
-    #(res['end_time']).time())
+    res['end_position_lat'], res['end_position_lon'],
+    (res['start_time']).time(), (res['end_time']).time()))
 
 # TODO Connect MongoDB with Spark, so we can directly distribute the data
 # we retrieved from Mongo in a RDD
 
-# Creating the RDD based ot the data retrieved from MongoDB
-
+# Time data are first converted to seconds and then mapped as coordinates
+# of a circle
 myRdd = sc.parallelize(users).cache()
-
-'''
-mapRdd = myRdd.map(lambda x: (x, 1))
-counts = mapRdd.reduceByKey(lambda a, b: a + b)
-frequent = counts.filter(lambda (a, b): b > 3)
-#frequent.collect()
-'''
+myRdd = myRdd.map(lambda x: (x[0], x[1], x[2], x[3],
+                             toCoordinates(toSeconds(x[4])),
+                             toCoordinates(toSeconds(x[5]))))
+# Normalize all the values between 0 and 1
+myRdd = myRdd.map(lambda (x1, x2, x3, x4, (x5, x6), (x7, x8)):
+                         (latNormalizer(x1), latNormalizer(x2),
+                          lonNormalizer(x3), lonNormalizer(x4),
+                          timeNormalizer(x5), timeNormalizer(x6),
+                          timeNormalizer(x7), timeNormalizer(x8)))
 
 # Function that implements the kmeans algorithm to group users requests
 def kmeans(NUM_OF_IT):
@@ -82,111 +117,63 @@ routes = []
 
 # Creation of route tuples of type (ID,(start, end))
 for res in route:
-    routes.append([res['_id'], (res['start_position_lat'],
-    res['start_position_lon'],  res['end_position_lat'],
-    res['end_position_lon'])])
+    #routes.append([res['_id'], (res['start_position_lat'],
+    #res['start_position_lon'],  res['end_position_lat'],
+    #res['end_position_lon'], res['StartTime'], res['EndTime'])])
+    for i in range(len(res['Waypoints']) - 1):
+        for j in range(i+1,len(res['Waypoints'])):
+            routes.append([res['_id'], (res['Waypoints'][i]['latitude'],
+            res['Waypoints'][i]['longitude'],
+            res['Waypoints'][j]['latitude'],
+            res['Waypoints'][j]['longitude'],
+            res['Waypoints'][i]['DptTime'],
+            res['Waypoints'][j]['DptTime'])])
 
+# Time data are first converted to seconds and then mapped as coordinates
+# of a circle
 myRoutes = sc.parallelize(routes).cache()
+myRoutes = myRoutes.map(lambda (y,x): (y, (x[0], x[1], x[2], x[3],
+                             toCoordinates(toSeconds(x[4])),
+                             toCoordinates(toSeconds(x[5])))))
+# Normalize all the values between 0 and 1
+myRoutes = myRoutes.map(lambda (y, (x1, x2, x3, x4, (x5, x6), (x7, x8))):
+                         (y, (latNormalizer(x1), latNormalizer(x2),
+                              lonNormalizer(x3), lonNormalizer(x4),
+                              timeNormalizer(x5),timeNormalizer(x6),
+                              timeNormalizer(x7),timeNormalizer(x8))))
 
 # The function that calculate the distance from the given tuple to all the
 # cluster centroids and returns the minimum disstance
 def calculateDistance(tup1):
     current_route = numpy.array(tup1)
+    print current_route
     distances = []
     for i in selected_centroids:
         centroid = numpy.array(i)
         distances.append(numpy.linalg.norm(current_route - centroid))
-    return min(distances)
+    return distances
 
 # Calculating all the distances and sorting the results by ascending value
 # therefore smallest distance comes first
 routesDistances = myRoutes.map(lambda x: (x[0], calculateDistance(x[1])))
-sortRoute = routesDistances.map(lambda (x,y): (y,x)).sortByKey()
-finalArray = sortRoute.map(lambda (x,y): (y,x))
 
-top5Suggestions = finalArray.take(30)
+finalRecommendation = []
+# Appends a specific number of recommendations for each cluster
+for i in range(len(selected_centroids)):
+    sortRoute = routesDistances.map(lambda (x, y): (x, y[i]))
+    sortRoute = sortRoute.map(lambda (x,y): (y,x)).sortByKey()
+    finalArray = sortRoute.map(lambda (x,y): (y,x))
+    finalRecommendation.append(finalArray.take(NUMBER_OF_RECOMMENDATIONS))
 
-for sug in top5Suggestions:
-    print sug
+recommendations = []
+# Transforms the list of lists format into a simple list with all the
+# recommendations
+for sug in finalRecommendation:
+    for i in range(len(sug)):
+        recommendations.append(sug[i])
 
-#print routes 561652b473b2392c6954fb48
-             #561652b473b2392c6954fb48
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-def main():
-    conf = SparkConf().setAppName("pyspark test")
-    sc = SparkContext(conf=conf)
-
-    # Create an RDD backed by the MongoDB collection.
-    # MongoInputFormat allows us to read from a live MongoDB instance.
-    # We could also use BSONFileInputFormat to read BSON snapshots.
-    rdd = sc.newAPIHadoopRDD(
-        inputFormatClass='com.mongodb.hadoop.MongoInputFormat',
-        keyClass='org.apache.hadoop.io.Text',
-        valueClass='org.apache.hadoop.io.MapWritable',
-        conf={
-            'mongo.input.uri': 'mongodb://localhost:27017/monad.TravelRequest'
-        }
-    )
-
-    # Save this RDD as a Hadoop "file".
-    # The path argument is unused; all documents will go to "mongo.output.uri".
-    rdd.saveAsNewAPIHadoopFile(
-        path='file:///this-is-unused',
-        outputFormatClass='com.mongodb.hadoop.MongoOutputFormat',
-        keyClass='org.apache.hadoop.io.Text',
-        valueClass='org.apache.hadoop.io.MapWritable',
-        conf={
-            'mongo.output.uri': 'mongodb://localhost:27017/output.collection'
-        }
-    )
-
-    # We can also save this back to a BSON file.
-    rdd.saveAsNewAPIHadoopFile(
-        path='hdfs://localhost:8020/user/spark/bson-demo',
-        outputFormatClass='com.mongodb.hadoop.BSONFileOutputFormat',
-        keyClass='org.apache.hadoop.io.Text',
-        valueClass='org.apache.hadoop.io.MapWritable'
-    )
-
-if __name__ == '__main__':
-    main()
-'''
+# Prints the recommendations
+# TODO Change that to return the final recommendations instead of just printing
+for sug in recommendations:
+    print TimeTable.find_one({"_id": sug[0]}, {"StartBusstop":1, "EndBusstop":1,
+                              "StartTime":1, "EndTime":1})
