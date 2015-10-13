922cb554daeef2ad53a8c887955d9765c327e58d
Jens Rosén <rosen.jens@gmail.com>
2015-10-13 17:05:16 +0200

    change some layout in the file

--- LookAhead/main.py
+++ LookAhead/main.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
 """Copyright 2015 Ericsson AB
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the 
+License at
 
 http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
 """
 import struct
 import time
 from dbConnection import DB
 from fitness import Fitness
-from deap import base
-from deap import creator
-from deap import tools
+from deap import base, creator, tools
 from itertools import repeat
 from collections import Sequence
 # Initialize the look ahead class
