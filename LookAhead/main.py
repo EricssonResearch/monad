70fa6e82a143a8a3c3e1022b1d1fd1c76c55fc44
ZiringTawfique <almanzt@hotmail.com>
2015-10-14 13:55:12 +0200

    re structure of code

--- LookAhead/main.py
+++ LookAhead/main.py
@@ -19,32 +19,50 @@ import toolBox
 import random
 # Variables
 MUTPB = 0.5
-# Generate the population
-pop = toolBox.toolbox.population()
-# Evaluate the entire population
-fitnesses = list(map(toolBox.toolbox.evaluate, pop))
-for ind, fit in zip(pop, fitnesses):
-    ind.fitness.values = fit
-# Cloning the population to select them based on their fitness
-offspring = toolBox.toolbox.select(pop, len(pop))
-offspring = list(map(toolBox.toolbox.clone, offspring))
-# Apply crossover
-# Testing mutation
-for mutant in pop:
-    if random.random() < MUTPB:
-        toolBox.toolbox.mutate(mutant)
-        del mutant.fitness.values
-'''
-invalids = [ind for ind in pop if not ind.fitness.valid]
-fitnesses = toolBox.toolbox.map(toolBox.toolbox.evaluate, invalids)
-for ind, fit in zip(invalids, fitnesses):
-    ind.fitness.values = fit
 
-'''
-print " Mutation done"
-for child1, child2 in zip(pop[::2], pop[1::2]):
-    print "Child 1"
-    print len(child1)
-    print "Child 2"
-    print len(child2)
-    # print(toolbox.mate(child1, child2))
+
+def main():
+    # Generate the population
+    pop = toolBox.toolbox.population()
+    # Evaluate the entire population
+    fitnesses = list(map(toolBox.toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+
+    # Cloning the population to select them based on their fitness
+    offspring = toolBox.toolbox.select(pop, len(pop))
+    offspring = list(map(toolBox.toolbox.clone, offspring))
+
+    # Calling the crossover function
+    crossover(offspring)
+
+
+    '''
+    invalids = [ind for ind in pop if not ind.fitness.valid]
+    fitnesses = toolBox.toolbox.map(toolBox.toolbox.evaluate, invalids)
+    for ind, fit in zip(invalids, fitnesses):
+        ind.fitness.values = fit
+    '''
+
+
+def crossover(offspring):
+    # Apply Crossover
+    for child1, child2 in zip(offspring[::2], offspring[1::2]):
+        print "Child 1"
+        print len(child1)
+        print "Child 2"
+        print len(child2)
+        print(toolBox.toolbox.mate(child1, child2))
+
+
+def mutation(offspring):
+     # Testing mutation
+     for mutant in offspring:
+        if random.random() < MUTPB:
+            toolBox.toolbox.mutate(mutant)
+            del mutant.fitness.values
+
+
+
+if __name__ == '__main__':
+    main()
