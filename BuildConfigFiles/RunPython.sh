2617fc30c377989dfae6ffe31685689d25711ef7
smavrakis <smavrakis3@hotmail.com>
2015-10-15 11:24:44 +0200

    Jenkins python testing fixes

--- BuildConfigFiles/RunPython.sh
+++ BuildConfigFiles/RunPython.sh
@@ -2,11 +2,7 @@
 
 cd /var/lib/jenkins/jobs/"ProjectCS 2015 Development"/workspace
 
-python -m compileall *.py
-python -m compileall */*.py
-python -m compileall */*/*.py
-python -m compileall */*/*/*.py
-python -m compileall */*/*/*/*.py
+python -m compileall -f -q ./
 
 #uncomment this when erlang script are uploaded
 #erl -compile **/*.erl
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 s='jenkins test'
-print s
+	print s
 
