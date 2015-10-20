e59fc8a07697bcda5f8bc9a2fdac7349344f4c9a
Daniel Llatas <danllatas@gmail.com>
2015-10-20 17:05:11 +0200

    DB class extended to query requests

--- LookAhead/main.py
+++ LookAhead/main.py
@@ -12,15 +12,10 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 """
-import random
-
 from deap import tools, algorithms
 import numpy
-
 from dbConnection import DB
 import toolBox
-from fitness import Fitness
-from datetime import datetime, timedelta
 
 # Variables
 MUTPB = 0.5
@@ -35,24 +30,24 @@ def main():
     pop = toolBox.toolbox.population(n=POPULATION_SIZE)
 
     hof = tools.HallOfFame(1)
-    
+
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
     stats.register("std", numpy.std)
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
 
-    pop, log = algorithms.eaSimple(pop, toolBox.toolbox, cxpb=CXPB, 
+    pop, log = algorithms.eaSimple(pop, toolBox.toolbox, cxpb=CXPB,
                                    mutpb=MUTPB, ngen=2, stats=stats,
                                    halloffame=hof, verbose=True)
 
-    ## Evaluate the entire population
-    #fitnesses = list(map(toolBox.toolbox.evaluate, pop))
-    #for ind, fit in zip(pop, fitnesses):
+    # Evaluate the entire population
+    # fitnesses = list(map(toolBox.toolbox.evaluate, pop))
+    # for ind, fit in zip(pop, fitnesses):
     #    ind.fitness.values = fit
 
-    ## Iterate trough a number of generations
-    #for g in range(NGEN):
+    # Iterate trough a number of generations
+    # for g in range(NGEN):
     #    print("-- Generation %i --" % g)
     #    # Select individuals based on their fitness
     #    offspring = toolBox.toolbox.select(pop, len(pop))
@@ -65,13 +60,13 @@ def main():
 
     #    invalidfitness(offspring)
 
-    ## The Best Individual found
+    # The Best Individual found
     best_ind = tools.selBest(pop, 1)[0]
     print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
     generateTimeTable(best_ind)
 
 
-#def crossover(offspring):
+# def crossover(offspring):
 #    # Apply Crossover
 #    for child1, child2 in zip(offspring[::2], offspring[1::2]):
 #        if random.random() < CXPB:
@@ -80,7 +75,7 @@ def main():
 #            del child2.fitness.values
 #
 #
-#def mutation(offspring):
+# def mutation(offspring):
 #    # Testing mutation
 #    for mutant in offspring:
 #         if random.random() < MUTPB:
@@ -88,7 +83,7 @@ def main():
 #             del mutant.fitness.values
 #
 #
-#def invalidfitness(offspring):
+# def invalidfitness(offspring):
 #    # Evaluate the individuals with an invalid fitness
 #    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
 #    fitnesses = map(toolBox.toolbox.evaluate, invalid_ind)
@@ -111,9 +106,8 @@ def main():
 
 def generateTimeTable(individual):
     databaseClass = DB()
-    timetable  = databaseClass.generateTripTimeTable(individual)
+    timetable = databaseClass.generateTripTimeTable(individual)
     databaseClass.insertTimeTable(timetable)
-
 
 
 if __name__ == '__main__':
