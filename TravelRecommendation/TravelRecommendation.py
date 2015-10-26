e1b794d11a7c4bfb36d988378a0ce6950f381abb
csioa <babispasg@gmail.com>
2015-10-26 15:56:17 +0100

    Modified travel recommendation system to work for multiple users

--- TravelRecommendation/TravelRecommendation.py
+++ TravelRecommendation/TravelRecommendation.py
@@ -14,6 +14,7 @@ specific language governing permissions and limitations under the License.
 import numpy
 import math
 import datetime
+from bson.objectid import ObjectId
 from pyspark import SparkContext
 from pymongo import MongoClient
 from pyspark.mllib.clustering import KMeans, KMeansModel
@@ -30,16 +31,10 @@ MIN_COORDINATE = -13750
 MAX_COORDINATE = 13750
 CIRCLE_CONVERTER = math.pi / 43200
 NUMBER_OF_RECOMMENDATIONS = 1
-users = []
-routes = []
-recommendations = []
-finalRecommendation = []
-selected_centroids = []
-routesDistances = []
-to_return = []
-userId = 123
 client = MongoClient()
 db = client.monad
+client2 = MongoClient('130.238.15.114')
+db2 = client2.monad
 
 def timeApproximation(lat1, lon1, lat2, lon2):
     point1 = (lat1, lon1)
@@ -47,36 +42,38 @@ def timeApproximation(lat1, lon1, lat2, lon2):
     distance = vincenty(point1, point2).kilometers
     return int(round(distance / 10 * 60))
 
-def dataBaseConnection():
-    # TODO Connect MongoDB with Spark, so we can directly distribute the data
-    # we retrieved from Mongo in a RDD
-    #client = MongoClient("130.238.15.114",27017)
+def retrieveRequests():
     TravelRequest = db.TravelRequest
-    TimeTable = db.TimeTable
-    return TravelRequest, TimeTable
+    return TravelRequest
 
-def populateFromDatabase(TravelRequest, TimeTable):
+def retrieveTimeTable():
+    TimeTable = db.TimeTable
+    return TimeTable
+
+def populateRequests(TravelRequest):
     results = TravelRequest.find()
     for res in results:
         dist = timeApproximation(res['start_position_lat'],
                                  res['start_position_lon'],
                                  res['end_position_lat'],
                                  res['end_position_lon'])
-        if res['start_time'] == "":
-            users.append((res['start_position_lat'], res['start_position_lon'],
+        if res['start_time'] == "null":
+            users.append((res['username'],(res['start_position_lat'], res['start_position_lon'],
             res['end_position_lat'], res['end_position_lon'],
             (res['end_time'] - datetime.timedelta(minutes = dist)).time(),
-            (res['end_time']).time()))
-        elif res['end_time' ] == "":
-            users.append((res['start_position_lat'], res['start_position_lon'],
+            (res['end_time']).time())))
+        elif res['end_time'] == "null":
+            users.append((res['username'],(res['start_position_lat'], res['start_position_lon'],
             res['end_position_lat'], res['end_position_lon'],
             (res['start_time']).time(),
-            (res['start_time'] + datetime.timedelta(minutes = dist)).time()))
+            (res['start_time'] + datetime.timedelta(minutes = dist)).time())))
         else:
-            users.append((res['start_position_lat'], res['start_position_lon'],
+            users.append((res['username'],(res['start_position_lat'], res['start_position_lon'],
             res['end_position_lat'], res['end_position_lon'],
             (res['start_time']).time(),
-            (res['end_time']).time()))
+            (res['end_time']).time())))
+
+def populateTimeTable(TimeTable):
     route = TimeTable.find()
     for res in route:
         for i in range(len(res['Waypoints']) - 1):
@@ -117,21 +114,21 @@ def lonNormalizer(value):
     return new_value
 
 # Function that implements the kmeans algorithm to group users requests
-def kmeans(iterations):
+def kmeans(iterations, theRdd):
     def error(point):
         center = clusters.centers[clusters.predict(point)]
         return sqrt(sum([x**2 for x in (point - center)]))
-    clusters = KMeans.train(myRdd, iterations, maxIterations=10,
+    clusters = KMeans.train(theRdd, iterations, maxIterations=10,
             runs=10, initializationMode="random")
-    WSSSE = myRdd.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+    WSSSE = theRdd.map(lambda point: error(point)).reduce(lambda x, y: x + y)
     return WSSSE, clusters
 
 # Function that runs iteratively the kmeans algorithm to find the best number
 # of clusters to group the user's request
-def optimalk():
+def optimalk(theRdd):
     results = []
     for i in range(NUM_OF_IT):
-        results.append(kmeans(i+1)[0])
+        results.append(kmeans(i+1, theRdd)[0])
     optimal = []
     for i in range(NUM_OF_IT-1):
         optimal.append(results[i] - results[i+1])
@@ -151,7 +148,7 @@ def calculateDistance(tup1):
     return distances
 
 def removeDuplicates(alist):
-    return list(set(map(lambda x: x[0], alist)))
+    return list(set(map(lambda (x, y): x, alist)))
 
 def recommendationsToReturn(alist):
     for sug in alist:
@@ -181,29 +178,34 @@ def recommendationsToDB(alist):
 
 def insertToDB(user, recs):
     db.TravelRecommendation.delete_many({"userId": user})
+    #db2.TravelRecommendation.delete_many({"userId": user})
+    o = ObjectId()
+    oo = str(o)
     new_record = {
+        "_id" : o,
+        "recId" : oo,
         "userId" : user,
         "recommendations": recs
     }
     db.TravelRecommendation.insert(new_record)
+    #db2.TravelRecommendation.insert(new_record)
+
+def emptyPastRecommendations():
+    db.TravelRecommendation.drop()
+    #db2.TravelRecommendation.drop()
 
 if __name__ == "__main__":
+
+    userIds = []
+    users = []
+    routes = []
+    userIds = []
+
     sc = SparkContext()
-    req, time_t = dataBaseConnection()
-    populateFromDatabase(req, time_t)
 
-    myRdd = sc.parallelize(users).cache()
+    time_t = retrieveTimeTable()
+    populateTimeTable(time_t)
     myRoutes = sc.parallelize(routes).cache()
-
-    myRdd = (myRdd.map(lambda x: (x[0], x[1], x[2], x[3],
-                                 toCoordinates(toSeconds(x[4])),
-                                 toCoordinates(toSeconds(x[5]))))
-                  .map(lambda (x1, x2, x3, x4, (x5, x6), (x7, x8)):
-                                (latNormalizer(x1), lonNormalizer(x2),
-                                 latNormalizer(x3), lonNormalizer(x4),
-                                 timeNormalizer(x5), timeNormalizer(x6),
-                                 timeNormalizer(x7), timeNormalizer(x8))))
-
     myRoutes = (myRoutes.map(lambda (y,x): (y, (x[0], x[1], x[2], x[3],
                                  toCoordinates(toSeconds(x[4])),
                                  toCoordinates(toSeconds(x[5])))))
@@ -213,21 +215,45 @@ if __name__ == "__main__":
                                  timeNormalizer(x5),timeNormalizer(x6),
                                  timeNormalizer(x7),timeNormalizer(x8)))))
 
-    selected_centroids = kmeans(optimalk())[1].centers
-    routesDistances = myRoutes.map(lambda x: (x[0], calculateDistance(x[1])))
+    req = retrieveRequests()
+    populateRequests(req)
+    initialRdd = sc.parallelize(users).cache()
+    userIdsRdd = (initialRdd.map(lambda (x,y): (x,1))
+                           .reduceByKey(lambda a, b: a + b)
+                           .collect())
 
-    for i in range(len(selected_centroids)):
-        sortRoute = (routesDistances.map(lambda (x, y): (x, y[i]))
-                                    .map(lambda (x,y): (y,x)).sortByKey()
-                                    .map(lambda (x,y): (y,x)))
-        finalRecommendation.append(sortRoute.take(NUMBER_OF_RECOMMENDATIONS))
+    for user in userIdsRdd:
+        userIds.append(user[0])
 
-    for sug in finalRecommendation:
-        for i in range(len(sug)):
-            recommendations.append(sug[i])
+    emptyPastRecommendations()
 
-    recommendations = removeDuplicates(recommendations)
-    recommendationsToReturn(recommendations)
+    for userId in userIds:
+        recommendations = []
+        finalRecommendation = []
+        selected_centroids = []
+        routesDistances = []
+        to_return = []
+        myRdd = initialRdd.filter(lambda (x,y): x == userId).map(lambda (x,y): y)
+        myRdd = (myRdd.map(lambda x: (x[0], x[1], x[2], x[3],
+                                     toCoordinates(toSeconds(x[4])),
+                                     toCoordinates(toSeconds(x[5]))))
+                      .map(lambda (x1, x2, x3, x4, (x5, x6), (x7, x8)):
+                                    (latNormalizer(x1), lonNormalizer(x2),
+                                     latNormalizer(x3), lonNormalizer(x4),
+                                     timeNormalizer(x5), timeNormalizer(x6),
+                                     timeNormalizer(x7), timeNormalizer(x8))))
+        selected_centroids = kmeans(optimalk(myRdd), myRdd)[1].centers
+        routesDistances = myRoutes.map(lambda x: (x[0], calculateDistance(x[1])))
+        for i in range(len(selected_centroids)):
+            sortRoute = (routesDistances.map(lambda (x, y): (x, y[i]))
+                                        .map(lambda (x,y): (y,x)).sortByKey()
+                                        .map(lambda (x,y): (y,x)))
+            finalRecommendation.append(sortRoute.take(NUMBER_OF_RECOMMENDATIONS))
+        for sug in finalRecommendation:
+            for i in range(len(sug)):
+                recommendations.append(sug[i])
 
-    recs = recommendationsToDB(to_return)
-    insertToDB(userId, recs)
+        recommendations = removeDuplicates(recommendations)
+        recommendationsToReturn(recommendations)
+        recs = recommendationsToDB(to_return)
+        insertToDB(userId, recs)
