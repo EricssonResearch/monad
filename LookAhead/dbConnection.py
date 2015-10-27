248d6707abf1f5255aa49d5b8830331357e40e1c
Daniel Llatas <danllatas@gmail.com>
2015-10-27 14:04:02 +0100

    Improved performance for average time function

--- LookAhead/dbConnection.py
+++ LookAhead/dbConnection.py
@@ -69,7 +69,7 @@ class DB():
 
     # These function will be called for every gene in order to get the difference
     def getTravelRequestBetween(self, start, end):
-        req = self.db.TravelRequest.find({ "StartTime": {"$gte": start, "$lt": end}})
+        req = self.db.TravelRequest.find({"StartTime": {"$gte": start, "$lt": end}})
         return req
 
     def getTravelRequestSummary(self, start, end):
@@ -79,20 +79,18 @@ class DB():
         reduce = "function(curr, result) { result.count++; }"
         # req = self.db.TravelRequest.group(keyf, condition, initial, reduce)
         req = self.db.TravelRequestLookAhead.group(keyf, condition, initial, reduce)
-        req = sorted(req, key=itemgetter("hour","minute"))
+        req = sorted(req, key=itemgetter("hour", "minute"))
         return req
 
     def getTravelRequestSummary2(self, start, end, busStop):
-
         keyf = "function(doc) { return { startBusStop: doc.startBusStop, hour: doc.startTime.getHours(), minute: doc.startTime.getMinutes()};}"
-        condition = {"startTime": {"$gte": start, "$lt": end}, "startBusStop": {"$eq": busStop} }
+        condition = {"startTime": {"$gte": start, "$lt": end}, "startBusStop": {"$eq": busStop}}
         initial = {"count": 0}
         reduce = "function(curr, result) { result.count++; }"
         # req = self.db.TravelRequest.group(keyf, condition, initial, reduce)
         req = self.db.TravelRequestLookAhead.group(keyf, condition, initial, reduce)
-        req = sorted(req, key=itemgetter("hour","minute"))
-
-        return req        
+        req = sorted(req, key=itemgetter("hour", "minute"))
+        return req
 
     """
         req = []
