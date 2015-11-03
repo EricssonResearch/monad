dd66382b21519b4cd5cb9dbe395b54365e65de03
hkominos <hkominos@gmail.com>
2015-11-04 00:18:13 +0100

    google messaging scafold added

---
+++ MoNADClient/app/src/main/java/se/uu/csproject/monadclient/googlecloudmessaging/MyInstanceIDListenerService.java
@@ -0,0 +1,32 @@
+package se.uu.csproject.monadclient.googlecloudmessaging;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.gms.iid.InstanceID;
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+import se.uu.csproject.monadclient.Authentication;
+
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+
+    private static final String TAG = "MyInstanceIDLS";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
+    // [START refresh_token]
+
+
+    @Override
+    public void onTokenRefresh() {
+        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+        Intent intent = new Intent(this, Authentication.class);
+        startService(intent);
+    }
+    // [END refresh_token]
+}
