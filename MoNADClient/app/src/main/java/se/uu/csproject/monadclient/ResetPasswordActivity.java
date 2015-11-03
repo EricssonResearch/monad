d66167dca7adda2f768c96d69ee3b817cc0313e0
madhu <chimatamadhu7@gmail.com>
2015-11-03 16:32:37 +0100

    Update Notifications

--- MoNADClient/app/src/main/java/se/uu/csproject/monadclient/ResetPasswordActivity.java
+++ MoNADClient/app/src/main/java/se/uu/csproject/monadclient/ResetPasswordActivity.java
@@ -1,19 +1,16 @@
 package se.uu.csproject.monadclient;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
