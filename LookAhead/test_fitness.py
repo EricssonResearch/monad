758403a594d665f6b6f12c8ce11cad68c271c7e5
huangtingwei <grace.htw@gmail.com>
2015-11-26 15:22:37 +0100

    fix building error

--- LookAhead/test_fitness.py
+++ LookAhead/test_fitness.py
@@ -12,10 +12,13 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 import unittest
+import sys
+sys.path.append('../OpenStreetMap')
 
 import toolBox
 from fitness import Fitness
 from datetime import datetime, timedelta
+from routeGenerator import coordinates_to_nearest_stops, get_route
 
 # Variables
 MUTPB = 0.5
