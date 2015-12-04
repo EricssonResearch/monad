38fa9306024e47ef4ea044f07825ffa5f06b56af
Daniel Llatas <danllatas@gmail.com>
2015-12-04 16:33:48 +0100

    New bus trips are generated according to the weather

--- LookAhead/weather.py
+++ LookAhead/weather.py
@@ -19,6 +19,7 @@ sys.path.append('../OpenStreetMap')
 from datetime import date
 from datetime import timedelta
 from dbConnection import DB
+from fitness import Fitness
 from routeGenerator import string_to_coordinates
 from operator import itemgetter
 
@@ -38,7 +39,7 @@ class Weather(object):
     def __init__(self):
         # super(Weather, self).__init__()
         # self.arg = arg
-        self.formatBusTrip(self.findTrip(self.getWeather(-1, Weather.address, Weather.apiKey), Weather.conditions))
+        self.processWeather(self.getAffectedTrip(self.getWeather(Weather.address, Weather.apiKey, -1), Weather.conditions))
 
     def getCoordinates(self, address):
         """Uses the Route Generator to search for an address"""
@@ -74,7 +75,7 @@ class Weather(object):
         return weather.count()
 
     def insertWeather(self, address, apiKey, days):
-        """Calls API to get weather information
+        """ Calls API to get weather information
         """
         db = DB()
         coordinates = self.getCoordinates(address)
@@ -84,14 +85,16 @@ class Weather(object):
                 weather = {'icon': data.icon, 'time': data.time, 'temperature': data.temperature}
                 db.insertWeather(weather)
 
-    def getWeather(self, days, address, apiKey):
-        """
+    def getWeather(self, address, apiKey, days):
+        """ This is the function to be called if someone wants to know the weather :D
         """
         if self.countWeather(self.queryWeather(self.setQueryDay(days))) == 0:
             self.insertWeather(address, apiKey, days)
         return self.queryWeather(self.setQueryDay(days))
 
-    def findTrip(self, weather, conditions):
+    def getAffectedTrip(self, weather, conditions):
+        """ Looks trips on DB that are gonna be affected by the weather
+        """
         trip = []
         db = DB()
         # Running trough the weather
@@ -107,91 +110,108 @@ class Weather(object):
         return trip
 
     def evaluateWeather(self, icon, temperature, time, conditions):
+        """ Evaluates if a result from the weather is part of a 
+        special weather condition
+        """
         if icon in conditions:
             return True
         else:
             return False
 
-    def formatBusTrip(self, trip):
+    def processWeather(self, trip):
+        """ This functions is like a main process.
+        From here, functions to modify the timetable and bustrips are called
+        """
+        if len(trip) > 0:
+            # self.modifyTimeTable(trip)  # This function deletes all the busTrips, and its references, also notifies users
+            self.generateBusTrip(trip)  # Generates new busTrips and updates the timetable
+
+    def modifyTimeTable(self, trip):
+        timetable = []
+        busId = []
         busTrip = []
+        db = DB()
         for i in xrange(len(trip)):
             for j in trip[i][3]:
                 busTrip.append([j["line"], j["_id"]])
         busTrip = sorted(busTrip, key=itemgetter(0))
-        self.searchTimeTable(busTrip)
-
-    def searchTimeTable(self, busTrip):
-        timetable = []
-        busId = []
         line = busTrip[0][0]
         busId.append(busTrip[0][1])
-        db = DB()
         for i in xrange(1, len(busTrip)):
             if line != busTrip[i][0]:
                 timetable = db.selectTimeTablebyBusTrip(busId)
-                timetable2 = self.processSomething(timetable, busId)
-                db.updateTimetable(timetable2[0], timetable2[1])
-                # db.deleteBusTrip(timetable2[1])
+                newTimetable = self.generateTimetable(timetable, busId)
+                db.updateTimetable(newTimetable[0], newTimetable[1], newTimetable[2], newTimetable[3])
+                self.deleteBusTrip(newTimetable[3])
                 # notifyUsers(timetable2[1])
                 busId = []
             line = busTrip[i][0]
             busId.append(busTrip[i][1])
         timetable = db.selectTimeTablebyBusTrip(busId)
-        timetable2 = self.processSomething(timetable, busId)
-        db.updateTimetable(timetable2[0], timetable2[1])
+        newTimetable = self.generateTimetable(timetable, busId)
+        db.updateTimetable(newTimetable[0], newTimetable[1], newTimetable[2], newTimetable[3])
+        self.deleteBusTrip(newTimetable[3])
+        # notifyUsers(timetable2[1])
 
-    def processSomething(self, timetable, busId):
+    def generateTimetable(self, timetable, busId):
+        '''
+        '''
         for t in timetable:
-            tt = [t["_id"], self.diff(t["timetable"], busId)]
+            tt = [t["_id"], t["date"], t["line"], self.diff(t["timetable"], busId)]
         return tt
 
     def diff(self, a, b):
         b = set(b)
         return [aa for aa in a if aa not in b]
 
-    def evaluateTrip(self, trip):
+    def deleteBusTrip(self, busTrip):
+        db = DB()
+        for bt in busTrip:
+            db.deleteBusTrip(bt)
+
+    def generateBusTrip(self, trip):
+        fitness = Fitness()
+        db = DB()
         count = 0
         chromosome = []
         line = None
-        db = DB()
         # Loop trough the whole trip structure
         # trip structure: icon, temperature, time and trips cursor
         for i in xrange(len(trip)):
             # Loop trough trips cursor
             # trips cursor: startTime, line and capacity
             for j in trip[i][3]:
-                # print j["_id"]
-                print j["_id"]
-                selectTimeTablebyBusTrip
-                for reg in db.selectTimeTablebyBusTrip([j["_id"]]):
-                    print reg
-                    print "======"
-                '''
                 if line is None:
                     line = j["line"]
                     startTime = j["startTime"]
                     capacity = j["capacity"]
                 if line != j["line"]:
-                    # Divided 60 by that number  to get frequency
-                    if trip[i][0] in Weather.conditions:
-                        # Calculate frequency on that hour
-                        frequency = int(round(60/count))
-                        # Change the frequency of the buses
-                        # >> freq, less buses
-                        # << freq, more buses
-                        frequency = frequency - 2
-                        if frequency < 0:
-                            frequency = 5
-                    chromosome.append([line, capacity, frequency, startTime])
-                    # print j["startTime"], j["line"], j["capacity"]
-                    # time = [trip[i][2],
+                    chromosome.append([line, capacity, self.calculateFrequency(count), startTime])
                     startTime = j["startTime"]
                     count = 0
                 count = count + 1
                 line = j["line"]
-                '''
-            # print "======"
-        # print chromosome
+        chromosome.append([line, capacity, self.calculateFrequency(count), startTime])
+        # It will generate one chromosome per each bus line
+        # As long as the conditions are not good, the frequency
+        # will be the same on a particular line
+        individual = fitness.genTimetable(chromosome)
+        print individual
+        # db.insertBusTrip2(individual)  # Increase functionality to update on the timetable
+
+    def calculateFrequency(self, count):
+        minutes = 60
+        factor = 2
+        minFreq = 5
+        # Calculate frequency: divided 60 by count
+        frequency = int(round(minutes/count))
+        # Change buses frequency: >freq <buses | <freq >buses
+        frequency = frequency - factor
+        # if frequency <= 0:
+        if frequency <= minFreq:
+            frequency = minFreq
+        return frequency
+
 
 if __name__ == '__main__':
     Weather()
