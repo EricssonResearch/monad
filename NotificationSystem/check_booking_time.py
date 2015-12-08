43d8fae22afb8fbaff0da83a0cf421a584e1606d
pinac0099 <pinac0099@gmail.com>
2015-12-08 15:01:37 +0100

    NotificationSystem: Changes related to booking reminders

--- NotificationSystem/check_booking_time.py
+++ NotificationSystem/check_booking_time.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 import datetime
-
 import requests
 import json
+import time
 
 AUTHENTICATION_MODULE_HOST = 'http://130.238.15.114:'
 AUTHENTICATION_MODULE_PORT = '9999'
@@ -35,42 +35,45 @@ def create_notification(user_id, partial_trips, icon_id):
     # print user_trip
     user_trips_list = parse_user_trip(list(), user_trip)
     notification['partialTrips'] = user_trips_list
-    notification['text'] = "Alert: Bus {} is arriving at {}".format(line, 
+    notification['text'] = "Alert: Bus {} is arriving at {}".format(line,
                             start_time)
     notification['time'] = datetime.datetime.now()
     notification['iconID'] = icon_id
-    print notification['userID'] , start_time 
-    return notification 
+    print notification['userID'] , start_time
+    return notification
 
 def main():
-    global user
     global mongo_client
-    global db
-    global bookings_collection
-    global notifications_collection
-    global user_trips
     mongo_client = MongoClient('130.238.15.114', 27017)
+    global db
     db = mongo_client.monad1
+    global bookings_collection
     bookings_collection = db.BookedTrip
-    user_trips = db.UserTrip
+    global notifications_collection
     notifications_collection = db.Notifications
-    now = datetime.datetime.now()
-    for booking in bookings_collection.find({'userID' : 1}):
-        list_trips = booking['partialTrips']
-        trip = user_trips.find_one({'_id' : list_trips[0]})
-        user_id = trip['userID']
-        start_time = trip['startTime']
-        dif = datetime.timedelta(minutes = 30)
-        if start_time - now < dif and start_time > now:
-            notification = create_notification(user_id, list_trips, 3)
-            try: 
-                notifications_collection.insert_one(notification)
-                send_notification_to_authentication(user_id, 'MoNAD', notification['text'])
-            except pymongo.errors.PyMongoError as e:
-                print e
-                
-            
-            
+    global user_trips
+    user_trips = db.UserTrip
+    
+    while True:
+        now = datetime.datetime.now()
+
+        for booking in bookings_collection.find({'notified' : False}):
+            list_trips = booking['partialTrips']
+            trip = user_trips.find_one({'_id' : list_trips[0]})
+            user_id = trip['userID']
+            start_time = trip['startTime']
+            dif = datetime.timedelta(minutes = 30)
+
+            if start_time - now < dif and start_time > now:
+                notification = create_notification(user_id, list_trips, 3)
+                try:
+                    notifications_collection.insert_one(notification)
+                    send_notification_to_authentication(user_id, 'MoNAD', notification['text'])
+                    bookings_collection.update_one({'_id' : booking['_id']}, {'$set' : {'notified' : True}})
+                except pymongo.errors.PyMongoError as e:
+                    print e
+
+        time.sleep(60)
 
 if __name__ == '__main__':
     main()
