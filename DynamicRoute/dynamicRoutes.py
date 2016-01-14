e68a01b16338670ea0a6e427933b9b16f0119693
HuangTingwei <grace.htw@gmail.com>
2016-01-14 10:55:06 +0100

    add Copyright in DR

--- DynamicRoute/dynamicRoutes.py
+++ DynamicRoute/dynamicRoutes.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2015 Ericsson AB
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+
+"""
 __author__ = 'mohammad'
 import random
 import string
@@ -10,7 +25,6 @@ import sys
 import math
 sys.path.append('../OpenStreetMap')
 sys.path.append('../LookAhead')
-
 from datetime import timedelta
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -29,6 +43,7 @@ class DynamicRoutes():
     # Constructor
     # Create Graph
     # Generate Dynamic Routes
+    # Store Dynamic Routes into Database
 
     # ---------------------------------------------------------------------------------------------------------------------------------------
     # Class variables
